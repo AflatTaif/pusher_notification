fc9eb4925c11a0d4c6f257935e22bffaca0f011e
Taifur Rahaman <107831777+AflatTaif@users.noreply.github.com>
2025-05-03 12:50:37 +0600

    Update notification_service.dart

--- lib/notification_service.dart
+++ lib/notification_service.dart
@@ -1,1 +1,32 @@
 
+
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
+import 'package:get/get.dart';
+import 'package:list_place_new/controllers/pusher_controller.dart';
+
+class FlutterNotificationService{
+  FlutterLocalNotificationsPlugin _flutterLocalNotificationsPlugin = FlutterLocalNotificationsPlugin();
+  void initNotification(){
+
+    var initializationSettingsAndroid =
+    AndroidInitializationSettings('flutter_logo');
+    var initializationSettings = InitializationSettings(
+        android: initializationSettingsAndroid);
+    _flutterLocalNotificationsPlugin.initialize(initializationSettings);
+  }
+
+  void showSimpleNotification({required String title,required String description,}) async {
+    var androidPlatformChannelSpecifics = AndroidNotificationDetails(
+        'your_channel_id', 'your_channel_name',
+        importance: Importance.max, priority: Priority.high, ticker: 'ticker');
+    var platformChannelSpecifics = NotificationDetails(
+        android: androidPlatformChannelSpecifics);
+    await _flutterLocalNotificationsPlugin.show(
+      0,
+      title.toString(),
+      description,
+      platformChannelSpecifics,
+    );
+  }
+
+}
